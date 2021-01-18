b34e8ced743cc884ddcd166fd24199def1f86f55
Fran <69062690+Fran2985@users.noreply.github.com>
2021-01-18 13:13:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ console.log(d.toLocaleDateString());
 
 client.on("ready", () => {
   console.log(`Ya estoy on pa´ en el ${client.user.tag}!`);
-    client.user.setActivity("En mantenimiento...", { type: "PLAYING"})
+    client.user.setActivity("No te entiendo una goma heroku", { type: "PLAYING"})
 
 });
 
