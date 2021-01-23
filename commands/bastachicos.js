b3cb55187e696efd5ad5b5f6f21a410af93527f4
Fran <69062690+Fran2985@users.noreply.github.com>
2021-01-23 01:39:24 -0300

    Update bastachicos.js

--- commands/bastachicos.js
+++ commands/bastachicos.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'bastachicos',
-    description: 'Tells the bot to reply with \"Pong!\"',
+    description: 'Basta Chicos',
     execute(msg, args) {
         msg.channel.send("https://cdn.discordapp.com/attachments/726437915197964338/802306022727352370/eyc9fJ.gif");
     }
