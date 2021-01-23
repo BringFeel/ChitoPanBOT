96b5aa1e73333ea072c3af9188e8fe2d9d54c3c3
Fran <69062690+Fran2985@users.noreply.github.com>
2021-01-23 00:41:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ console.log(os.cpus());
 console.log(os.totalmem());
 console.log(os.freemem())
 
-const os = require('os-utils');
-
 os.cpuUsage(function (v) {
   console.log('CPU Usage (%): ' + v);
 });
