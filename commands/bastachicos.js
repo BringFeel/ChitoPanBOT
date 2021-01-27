6daadfec2260cdd1f55d079a66df47b9abee92bc
Fran <69062690+Fran2985@users.noreply.github.com>
2021-01-27 01:45:36 -0300

    Update bastachicos.js

--- commands/bastachicos.js
+++ commands/bastachicos.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Basta Chicos',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/726437915197964338/802306022727352370/eyc9fJ.gif').setTimestamp();
+        embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/726437915197964338/802306022727352370/eyc9fJ.gif').setTimestamp().setFooter(`${msg.author.tag}`, `${message.author.avatarURL}`)();
         msg.channel.send(embed);
     }
 };
