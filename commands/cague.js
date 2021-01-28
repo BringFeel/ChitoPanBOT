46b977ff94632e3fcbabffa27a0611a58a6edbdc
Fran <69062690+Fran2985@users.noreply.github.com>
2021-01-28 15:08:32 -0300

    Update cague.js

--- commands/cague.js
+++ commands/cague.js
@@ -1,1 +1,11 @@
+const Discord = require('discord.js');
 
+module.exports = {
+    name: 'cague',
+    description: 'cague',
+    execute(msg, args) {
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/726437915197964338/804412367168471090/y2mate.com_-_deigamer_cague_xD_360p.mp4').setTimestamp();
+        msg.channel.send(embed);
+    }
+};
