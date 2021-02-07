ddab378a8ae7a6144da503457561341d669747bd
Fran <69062690+Fran2985@users.noreply.github.com>
2021-02-07 11:36:54 -0300

    Update boda.js

--- commands/boda.js
+++ commands/boda.js
@@ -1,1 +1,12 @@
+  
+const Discord = require('discord.js');
 
+module.exports = {
+    name: 'boda',
+    description: 'boda',
+    execute(msg, args) {
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/726437915197964338/804416882387976252/cague.gif').setTimestamp();
+        msg.channel.send(embed);
+    }
+};
