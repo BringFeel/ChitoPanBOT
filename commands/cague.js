8b2be1dd8b7025779e245f13736a9489c63babaa
Fran <69062690+Fran2985@users.noreply.github.com>
2021-02-07 11:09:41 -0300

    Update cague.js

--- commands/cague.js
+++ commands/cague.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'cague',
     description: 'cague',
     execute(msg, args) {
+        msg.channel.send(msg.author.String())
         const embed = new Discord.MessageEmbed();
         embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/726437915197964338/804416882387976252/cague.gif').setTimestamp();
         msg.channel.send(embed);
