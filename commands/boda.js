cb9f12c814c8e92be918eff802e1d56ed4537f28
Fran <69062690+Fran2985@users.noreply.github.com>
2021-02-11 10:41:32 -0300

    Update boda.js

--- commands/boda.js
+++ commands/boda.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'boda',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF0202').setFooter(client.user.avatarURL).setFooter("FranBOT#4390").setImage('https://cdn.discordapp.com/attachments/776612505093931098/809159523871752192/2021-02-10_17.29.58.png').setTimestamp();
+        embed.setColor('#FF0202').setAuthor(client.user.avatarURL).setFooter("FranBOT#4390").setImage('https://cdn.discordapp.com/attachments/776612505093931098/809159523871752192/2021-02-10_17.29.58.png').setTimestamp();
         msg.channel.send(embed);
     }
 };
