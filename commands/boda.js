c520a5a3bccbac6cb37710b22eafa29b71dd795d
Fran <69062690+Fran2985@users.noreply.github.com>
2021-02-11 11:35:35 -0300

    Update boda.js

--- commands/boda.js
+++ commands/boda.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'boda',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF0202').addField("Boda de Fran y Light","La boda se va a hacer el 14/02/2021").addField("Server:", "CobelMotion", true).addBlankField(true).addField("Versión:", "1.12.2 - 1.16.5 [1.16.5 recomendada]", true).setFooter("FranBOT#4390").setImage('https://cdn.discordapp.com/attachments/776612505093931098/809159523871752192/2021-02-10_17.29.58.png').setTimestamp();
+        embed.setColor('#FF0202').addField("Boda de Fran y Light","La boda se va a hacer el 14/02/2021").addField("Server:", "CobelMotion","Versión:", "1.12.2 - 1.16.5 [1.16.5 recomendada]", true).setFooter("FranBOT#4390").setImage('https://cdn.discordapp.com/attachments/776612505093931098/809159523871752192/2021-02-10_17.29.58.png').setTimestamp();
         msg.channel.send(embed);
     }
 };
