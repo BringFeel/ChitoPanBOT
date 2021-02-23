223f7ca747d6fde7f96c276da891bb8f37def5c4
Fran <69062690+Fran2985@users.noreply.github.com>
2021-02-23 14:57:17 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'avatar',
-    aliases: ["pong", "pingpong"],
+    aliases: ["av", "foto"],
     description: 'Muestra tu avatar o el de otro usuario',
     execute(msg, args) {
         const avatarEmbed = require('discord.js-avatar');
