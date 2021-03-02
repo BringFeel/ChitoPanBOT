19cfac92cbe32b8ac0dfa90bf2fd443d16798f30
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-01 23:49:52 -0300

    Create siri.js

---
+++ commands/siri.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    name: 'siri',
+    description: 'Siri fazendo barra! incrivel',
+    execute(msg, args) {
+        msg.channel.send("https://cdn.discordapp.com/attachments/669301749009350656/812144372794654740/ae6961f7be174dcba005caa82b26eda1.mp4");
+    }
+};
