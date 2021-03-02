6cd66bca844634a565364976d55522e77845bf6d
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-01 23:55:13 -0300

    Update siri.js

--- commands/siri.js
+++ commands/siri.js
@@ -3,6 +3,6 @@ module.exports = {
     name: 'siri',
     description: 'Siri fazendo barra! incrivel',
     execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/669301749009350656/812144372794654740/ae6961f7be174dcba005caa82b26eda1.mp4");
+        msg.channel.send("https://cdn.discordapp.com/attachments/779878778842513449/816141461430337546/siri.mp4");
     }
 };
