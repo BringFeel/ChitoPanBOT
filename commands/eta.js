3a66a27135acc67ed9a62d5abe857756048474b6
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-01 23:14:20 -0300

    Update eta.js

--- commands/eta.js
+++ commands/eta.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: '100',
-    description: 'Cien',
+    name: 'eta',
+    description: 'eta poooo',
     execute(msg, args) {
         msg.channel.send("https://cdn.discordapp.com/attachments/669301749009350656/812144372794654740/ae6961f7be174dcba005caa82b26eda1.mp4");
     }
