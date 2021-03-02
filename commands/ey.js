848edd36d070db3ef8b1570c95eb3cacf025d3c0
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-01 23:13:41 -0300

    Update ey.js

--- commands/ey.js
+++ commands/ey.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: '100',
-    description: 'Cien',
+    name: 'ey',
+    description: 'eyeyey',
     execute(msg, args) {
         msg.channel.send("https://cdn.discordapp.com/attachments/779878778842513449/816130764026282014/eyeyey.mp4");
     }
