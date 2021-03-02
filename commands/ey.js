282430c134a4944da989e5612f05fdfa4f4b2dbc
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-01 23:12:29 -0300

    Create ey.js

---
+++ commands/ey.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: '100',
+    description: 'Cien',
+    execute(msg, args) {
+        msg.channel.send("https://cdn.discordapp.com/attachments/779878778842513449/816130764026282014/eyeyey.mp4");
+    }
+};
