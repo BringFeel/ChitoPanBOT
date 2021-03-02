0a4f8cf92abf30d975b8ec4ce164201194dec276
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-01 23:10:51 -0300

    Create eta.js

---
+++ commands/eta.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: '100',
+    description: 'Cien',
+    execute(msg, args) {
+        msg.channel.send("https://cdn.discordapp.com/attachments/669301749009350656/812144372794654740/ae6961f7be174dcba005caa82b26eda1.mp4");
+    }
+};
