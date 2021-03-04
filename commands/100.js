9d74425b07b10664ae14b5b794e5dcbd0faa9369
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-04 16:42:19 -0300

    Update 100.js

--- commands/100.js
+++ commands/100.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: '100',
-    description: 'Cien',
+    description: 'Elaos de cien',
     execute(msg, args) {
         msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/817118630403375144/cuanto_valen_los_elaos_de_sien.mp4");
     }
