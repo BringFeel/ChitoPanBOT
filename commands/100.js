2a9bac248088a49d39617299e42ebbb1d1054e0c
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-04 16:38:16 -0300

    Update 100.js

--- commands/100.js
+++ commands/100.js
@@ -2,6 +2,6 @@ module.exports = {
     name: '100',
     description: 'Cien',
     execute(msg, args) {
-        msg.channel.send("https://www.cobel-motion.xyz/videos/cuanto-valen-los-helados-de-sien.mp4");
+        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/817118630403375144/cuanto_valen_los_elaos_de_sien.mp4");
     }
 };
