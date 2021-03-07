1d350c629c4a6dcca4e6a943481049e83cc9bbf6
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-07 17:14:30 -0300

    Create messirve.js

---
+++ commands/messirve.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'merssirve',
+    description: 'merssirve pa´',
+    execute(msg, args) {
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#00FFA9').setImage('https://cdn.discordapp.com/attachments/795478872597594164/818214522594525234/Messirve.jpg').setTimestamp();
+        msg.channel.send(embed);
+    }
+};
