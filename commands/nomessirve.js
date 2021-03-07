582f4bd22a8cd85b39b20a447862a8737a27dba4
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-07 17:15:37 -0300

    Create nomessirve.js

---
+++ commands/nomessirve.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'Nomerssirve',
+    description: 'Nomerssirve pibe',
+    execute(msg, args) {
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#6AFFCD').setImage('https://cdn.discordapp.com/attachments/795478872597594164/818214534539640862/No_Messirve.jpg').setTimestamp();
+        msg.channel.send(embed);
+    }
+};
