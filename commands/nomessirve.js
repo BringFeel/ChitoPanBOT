5264ab5b7616934b0d6f57c67ecfec7014fae3ef
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-07 17:25:00 -0300

    Update nomessirve.js

--- commands/nomessirve.js
+++ commands/nomessirve.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'nomessirve',
-    description: 'nomessirve pibe'',
+    description: 'nomessirve pibe',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
         embed.setColor('#6AFFCD').setImage('https://cdn.discordapp.com/attachments/795478872597594164/818214534539640862/No_Messirve.jpg').setTimestamp();
