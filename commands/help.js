8d5a288e1bb3baf31cea5aec4f787ff827487dc9
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-07 17:18:45 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Lista de los comandos disponibles.',
     execute(msg, args) {
         const commands = ['100', 'avatar', 'bastachicos', 'calmese', 'eu', 'furry',
-        'gay', 'help', 'invite', 'otaku', 'ping', 'puto', 'up', "cs", "callao", "bi", "eta", "ey", "siri", "merssirve", "nomerssirve"];
+        'gay', 'help', 'invite', 'otaku', 'ping', 'puto', 'up', "cs", "callao", "bi", "eta", "ey", "siri", "messirve", "nomessirve"];
         const embed = new Discord.MessageEmbed()
         .setTitle('Comandos')
         .setColor('#199057')
