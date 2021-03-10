387860b54b9018be8238ea27dec44544380e9ded
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-10 12:04:34 -0300

    Update poc.js

--- commands/poc.js
+++ commands/poc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'callao',
-    description: 'Callao lacra',
+    name: 'poc',
+    description: 'que poc?',
     execute(msg, args) {
         msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/819223321090326555/poc.mp4");
     }
