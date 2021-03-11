94c85ce4f67bede54e8aa667ee804e0288e75aa2
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-11 13:00:23 -0300

    Update otaku.js

--- commands/otaku.js
+++ commands/otaku.js
@@ -9,7 +9,7 @@ module.exports = {
             number = 256;
         }
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#000000').setTitle(`${number}% otaku`);
+        embed.setColor('#000000').setTitle(`Otaku`).setDescription(`${number}%`);
         msg.channel.send(embed);
     }
 };
