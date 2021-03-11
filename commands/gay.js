5ebe4182a96f89078052887051ee29592ff4acdf
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-11 14:20:54 -0300

    Update gay.js

--- commands/gay.js
+++ commands/gay.js
@@ -9,7 +9,7 @@ module.exports = {
             number = 325619;
         }
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FFAE00').setTitle(`Gay`).setDescription(`${number}%`);
+        embed.setColor('#FFAE00').setTitle(`Gay`).addField(msg.author.username, `${number}%`, true);
         msg.channel.send(embed);
     }
 };
