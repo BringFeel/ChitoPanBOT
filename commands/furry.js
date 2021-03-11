c75c8c674f5fadba522c76344475a2f101cd6ebb
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-11 14:21:21 -0300

    Update furry.js

--- commands/furry.js
+++ commands/furry.js
@@ -9,7 +9,7 @@ module.exports = {
             number = 512;
         }
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#002D56').setTitle(`Furry`).setDescription(`${number}%`);
+        embed.setColor('#002D56').setTitle(`Furry`).addField(msg.author.username, `${number}%`, true);
         msg.channel.send(embed);
     }
 };
