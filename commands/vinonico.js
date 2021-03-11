3f6a2ab7cbaed80561eb03d7fbdfb14eeacd3114
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-10 21:26:54 -0300

    Create vinonico.js

---
+++ commands/vinonico.js
@@ -0,0 +1,12 @@
+
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'vinonico',
+    description: 'son pollos pibes',
+    execute(msg, args) {
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/806436279877238804/819354753221918730/img2.rtve.jpg').setTimestamp();
+        msg.channel.send(embed);
+    }
+};
