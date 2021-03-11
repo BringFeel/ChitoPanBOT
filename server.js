58f1f6717f234063344ea2ad5d66bd5e299e1242
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-11 08:57:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ console.log(d.toLocaleDateString());
 
 client.on("ready", () => {
   console.log(`Ya estoy on pa´ en el ${client.user.tag}!`);
-  client.user.setActivity("+help | Fran + Light = Pareja Perfecta", { type: "PLAYING" })
+  client.user.setActivity("+help | +invite", { type: "PLAYING" })
   client.guilds.cache.each(guild => console.info(`Presente en el servidor "${guild.name}"`));
 });
 
