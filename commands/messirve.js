776a9135b8ec829238379ba787181dae16ac5548
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-12 15:49:33 -0300

    Update messirve.js

--- commands/messirve.js
+++ commands/messirve.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'messirve pa´',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#00FFA9').setImage('https://cdn.discordapp.com/attachments/795478872597594164/818214522594525234/Messirve.jpg').setTimestamp();
+        embed.setColor('#00FFA9').setTitle(`Messirve`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/818214522594525234/Messirve.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
