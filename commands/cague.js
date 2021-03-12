925c2e2d95e1405c25a155b7a49a7b1b14c9d1a0
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-12 15:50:56 -0300

    Update cague.js

--- commands/cague.js
+++ commands/cague.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'cague',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/726437915197964338/804416882387976252/cague.gif').setTimestamp();
+        embed.setColor('#FF0202').setTitle(`Cague`).setImage('https://cdn.discordapp.com/attachments/726437915197964338/804416882387976252/cague.gif').setTimestamp();
         msg.channel.send(embed);
     }
 };
