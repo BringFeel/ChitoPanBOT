eed171fdc5560735610b4bdffaf12e75dd7fcfdf
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-12 15:47:40 -0300

    Update vinonico.js

--- commands/vinonico.js
+++ commands/vinonico.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'son pollos pibes',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF0202').setImage('https://cdn.discordapp.com/attachments/806436279877238804/819354753221918730/img2.rtve.jpg').setTimestamp();
+        embed.setColor('#FF0202').setTitle(`Vino Nico`).setImage('https://cdn.discordapp.com/attachments/806436279877238804/819354753221918730/img2.rtve.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
