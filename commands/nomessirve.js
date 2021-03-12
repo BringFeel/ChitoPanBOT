9479657d22ed9f37d24f6f6778cd7adf61c6d7c3
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-12 15:49:12 -0300

    Update nomessirve.js

--- commands/nomessirve.js
+++ commands/nomessirve.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'nomessirve pibe',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#6AFFCD').setImage('https://cdn.discordapp.com/attachments/795478872597594164/818214534539640862/No_Messirve.jpg').setTimestamp();
+        embed.setColor('#6AFFCD').setTitle(`NoMessirve`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/818214534539640862/No_Messirve.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
