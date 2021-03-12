884d7c5f105a5486bbae62b31c1d6fbc6ddb276b
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-12 15:50:26 -0300

    Update clown.js

--- commands/clown.js
+++ commands/clown.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'se te cayó tu identificación pa´',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF9F53').setImage('https://cdn.discordapp.com/attachments/795478872597594164/818486074379862016/se_te_cayo_esto.png').setTimestamp();
+        embed.setColor('#FF9F53').setTitle(`Tomá pibe`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/818486074379862016/se_te_cayo_esto.png').setTimestamp();
         msg.channel.send(embed);
     }
 };
