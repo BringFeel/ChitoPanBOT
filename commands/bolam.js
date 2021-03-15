abef9253d98febf9a31780ed3ef90378a35b09f9
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-15 00:27:16 -0300

    Create bolam.js

---
+++ commands/bolam.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    name: 'bolam',
+    description: 'paloma',
+    execute(msg, args) {
+        msg.channel.send("https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_232801.jpg");
+    }
+};
