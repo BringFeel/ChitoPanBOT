4361d91d98716561f92ae9cc7bccd8e5f0f71f4e
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-14 23:06:55 -0300

    Create buendia.js
    QUE BUEN DIA

---
+++ commands/buendia.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'buendia',
+    description: 'Que buen día',
+    execute(msg, args) {
+        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/820839648241909800/Que_buen_dia_ta_como_pa_culiarse_a_un_mono.mp4");
+    }
+};
