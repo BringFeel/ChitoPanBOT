358375767102e03842289bc91457f7a52f8c6036
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-16 11:23:35 -0300

    Update bolam.js

--- commands/bolam.js
+++ commands/bolam.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'paloma',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#00FBFF').setTitle(`Bolam`).setImage('https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_23280Bolam').setTimestamp();
+        embed.setColor('#00FBFF').setTitle(`Bolam la paloma`).setImage('https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_23280Bolam').setTimestamp();
         msg.channel.send(embed);
     }
 };
