58b705ff08e33afaf03511a429b702280d8a8ce6
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-16 11:24:29 -0300

    Update bolam.js

--- commands/bolam.js
+++ commands/bolam.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'bolam',
-    description: 'paloma',
+    description: 'Paloma',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
         embed.setColor('#00FBFF').setTitle(`Bolam la paloma`).setImage('https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_23280Bolam').setTimestamp();
