0e4ba1ead5f44a60fb50a7b82c7fcd3a30e8caa0
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:16:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ console.log(d.toLocaleDateString());
 
 client.on("ready", () => {
   console.log(`Ya estoy on pa´ en el ${client.user.tag}!`);
-  client.user.setActivity("+help -- Light + Fran = ❤", { type: "PLAYING" })
+  client.user.setActivity("En mantenimiento...", { type: "PLAYING" })
   client.guilds.cache.each(guild => console.info(`Presente en el servidor "${guild.name}"`));
 });
 
