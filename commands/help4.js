cb4929220e21cee966f228db88d28d9a0d3b6afc
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:30:50 -0300

    Create help4.js

---
+++ commands/help4.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'help4',
+    description: 'Lista de los comandos disponibles.',
+    execute(msg, args) {
+        const commands = ["buendia", "jueves"];
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Comandos | 4/4')
+        .setColor('#199057')
+        .setDescription(`Comandos disponibles | ${commands.length} en total`)
+        .setFooter('Fran-BOT#4390', 'https://cdn.discordapp.com/avatars/779841907484262421/353e218ba30c538fea8684b62aedd13b.webp?size=4096')
+        .setTimestamp(new Date());
+        for (let i = 0; i < commands.length; i++) {
+            embed.addField(commands[i], client.commands.get(commands[i]).description);
+        }
+        msg.channel.send(embed);
+    }
+};
