451bf9f125bbe51ee65c07429a2eee8f52bda3e7
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:50:06 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,17 +6,17 @@ module.exports = {
 
     async run (client, message, args) {
 
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
-        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
+        if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send('You can\'t use that!')
+        if(!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.send('I don\'t have the right permissions.')
 
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        const member = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]);
 
-        if(!args[0]) return message.channel.send('Please specify a user');
+        if(!args[0]) return msg.channel.send('Please specify a user');
 
-        if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
-        if(!member.bannable) return message.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
+        if(!member) return msg.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
+        if(!member.bannable) return msg.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
 
-        if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
+        if(member.id === msg.author.id) return msg.channel.send('Bruh, you can\'t ban yourself!');
 
         let reason = args.slice(1).join(" ");
 
@@ -31,12 +31,12 @@ module.exports = {
         .setTitle('Member Banned')
         .setThumbnail(member.user.displayAvatarURL())
         .addField('User Banned', member)
-        .addField('Kicked by', message.author)
+        .addField('Kicked by', msg.author)
         .addField('Reason', reason)
         .setFooter('Time kicked', client.user.displayAvatarURL())
         .setTimestamp()
 
-        message.channel.send(banembed);
+        msg.channel.send(banembed);
 
 
     }
