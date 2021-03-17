cd668f3057affe79a0a02a5ec08c3005581426cf
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:20:59 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,9 +7,8 @@ module.exports = {
         const commands = ['100', 'avatar', 'bastachicos', 'calmese', 'eu', 'furry',
         'gay', 'help', 'invite', 'otaku', 'ping', 'puto', 'up', "cs", "callao", "bi", "eta", "ey", "siri", "messirve", "nomessirve", "clown", "poc", "vinonico", "buendia"];
         const embed = new Discord.MessageEmbed()
-        .setTitle('Comandos')
+        .setTitle('Comandos | 1/3')
         .setColor('#199057')
-        .setAuthor('FranBOT', client.user.avatarURL, 'https://www.cobel-motion.xyz/FranBOT.html')
         .setDescription(`Comandos disponibles | ${commands.length} en total`)
         .setFooter('Fran-BOT#4390', 'https://cdn.discordapp.com/avatars/779841907484262421/353e218ba30c538fea8684b62aedd13b.webp?size=4096')
         .setTimestamp(new Date());
