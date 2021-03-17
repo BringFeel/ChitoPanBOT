a6c132922af5a2ae1954ceab7975c69b0cb50257
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:29:28 -0300

    Create jueves.js

---
+++ commands/jueves.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'jueves',
+    description: 'Feliz jueves',
+    execute(msg, args) {
+        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/821767135600443442/Asuka__Feliz_Jueves_HD_60fps720P_HD.mp4");
+    }
+};
