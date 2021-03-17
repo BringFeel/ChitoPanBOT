030c41aaee44d41cc0313938c5d20f1a1f43bd86
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:29:58 -0300

    Update help3.js

--- commands/help3.js
+++ commands/help3.js
@@ -4,9 +4,9 @@ module.exports = {
     name: 'help3',
     description: 'Lista de los comandos disponibles.',
     execute(msg, args) {
-        const commands = ["eta", "ey", "siri", "messirve", "nomessirve", "clown", "poc", "vinonico", "buendia"];
+        const commands = ["eta", "ey", "siri", "messirve", "nomessirve", "clown", "poc", "vinonico"];
         const embed = new Discord.MessageEmbed()
-        .setTitle('Comandos | 3/3')
+        .setTitle('Comandos | 3/4')
         .setColor('#199057')
         .setDescription(`Comandos disponibles | ${commands.length} en total`)
         .setFooter('Fran-BOT#4390', 'https://cdn.discordapp.com/avatars/779841907484262421/353e218ba30c538fea8684b62aedd13b.webp?size=4096')
