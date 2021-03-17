d2ef035dfe630f58f1925c44c25ae297dc94b257
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:46:47 -0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,17 +1,43 @@
+const Discord = require('discord.js');
+
 module.exports = {
-    name: 'ban',
-    description: 'banea al que se hace el piola',
-    execute(msg, args) {
-        if (msg.member.hasPermission("BAN_MEMBERS")) {
-    if (msg.members.mentions.first()) {
-        try {
-            msg.members.mentions.first().ban();
-        } catch {
-            msg.reply("Dame permisos de ban wacho porque si no viste, como que no puedo banear" + msg.members.mentions.first());
-        }
-    } else {
-        msg.reply("No tenés permisos de ban pibe" + msg.members.mentions.first());
+    name: "ban",
+    description: "Kicks a member from the server",
+
+    async run (client, message, args) {
+
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
+
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+        if(!args[0]) return message.channel.send('Please specify a user');
+
+        if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
+        if(!member.bannable) return message.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
+
+        if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
+
+        let reason = args.slice(1).join(" ");
+
+        if(!reason) reason = 'Unspecified';
+
+        member.ban(`${reason}`).catch(err => { 
+          message.channel.send('Something went wrong')
+            console.log(err)
+        })
+
+        const banembed = new Discord.MessageEmbed()
+        .setTitle('Member Banned')
+        .setThumbnail(member.user.displayAvatarURL())
+        .addField('User Banned', member)
+        .addField('Kicked by', message.author)
+        .addField('Reason', reason)
+        .setFooter('Time kicked', client.user.displayAvatarURL())
+        .setTimestamp()
+
+        message.channel.send(banembed);
+
+
     }
 }
-    }
-};
