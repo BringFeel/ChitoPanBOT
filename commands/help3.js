a195622b180d2a6eca596d367fb2b7360520a54f
Fran <69062690+Fran2985@users.noreply.github.com>
2021-03-17 12:22:26 -0300

    Create help3.js

---
+++ commands/help3.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'help2',
+    description: 'Lista de los comandos disponibles.',
+    execute(msg, args) {
+        const commands = ['100', 'avatar', 'bastachicos', 'calmese', 'eu', 'furry',
+        'gay', 'help', 'invite', 'otaku', 'ping', 'puto', 'up', "cs", "callao", "bi", "eta", "ey", "siri", "messirve", "nomessirve", "clown", "poc", "vinonico", "buendia"];
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Comandos | 3/3')
+        .setColor('#199057')
+        .setDescription(`Comandos disponibles | ${commands.length} en total`)
+        .setFooter('Fran-BOT#4390', 'https://cdn.discordapp.com/avatars/779841907484262421/353e218ba30c538fea8684b62aedd13b.webp?size=4096')
+        .setTimestamp(new Date());
+        for (let i = 0; i < commands.length; i++) {
+            embed.addField(commands[i], client.commands.get(commands[i]).description);
+        }
+        msg.channel.send(embed);
+    }
+};
