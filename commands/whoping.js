8c14e354392387a7c021dcc72cb2ac2c3c7556bb
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-01 23:14:12 -0300

    Update whoping.js

--- commands/whoping.js
+++ commands/whoping.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'whoping',
-    description: 'quien chota pingueo',
+    description: 'quien chota pingueó',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#1500FF').setTitle(`Quien Chota Pingueo`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/827363440879206450/ping.jpg').setTimestamp();
+        embed.setColor('#1500FF').setTitle(`Quien Chota Pingueó`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/827363440879206450/ping.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
