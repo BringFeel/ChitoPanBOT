936818ab413e422cdfa7cda459d6f716096aae39
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-04 17:59:31 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
         .setTitle('Comandos | 1/4')
         .setColor('#199057')
         .setDescription(`Comandos disponibles | ${commands.length} en total`)
-        .setFooter('Fran-BOT#4390', 'https://cdn.discordapp.com/avatars/779841907484262421/353e218ba30c538fea8684b62aedd13b.webp?size=4096')
+        .setFooter('ChitoPanBOT#4390', 'https://cdn.discordapp.com/attachments/795478872597594164/828373150382161970/a.jpg')
         .setTimestamp(new Date());
         for (let i = 0; i < commands.length; i++) {
             embed.addField(commands[i], client.commands.get(commands[i]).description);
