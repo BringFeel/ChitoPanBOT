5a319b5301928ead4ad3b55fdb138173e502758e
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-04 18:04:41 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -34,8 +34,8 @@ module.exports = {
                 ],
                 timestamp: new Date(),
                 footer: {
-                    icon_url: "https://cdn.discordapp.com/avatars/779841907484262421/353e218ba30c538fea8684b62aedd13b.webp?size=4096",
-                    text: "Fran-BOT#4390"
+                    icon_url: "https://cdn.discordapp.com/attachments/795478872597594164/828373150382161970/a.jpg",
+                    text: "ChitoPanBOT#4390"
                 }
             }
         });
