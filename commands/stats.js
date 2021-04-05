c134d1033f98d36dede6dc2b1c767b22b8b4a8df
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-05 03:22:16 -0300

    Add files via upload

---
+++ commands/stats.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+os = require('os');
+
+module.exports = {
+    name: 'stats',
+    description: 'Estadisticas repiolas',
+    execute(msg, args) {
+        const embed = new Discord.MessageEmbed();
+        embed.setColor('#FF0202').setTitle(`Cague`)
+        .addField(`:desktop: System`, `${os.platform()}`, true)
+        .addField(`:gear: Arquitectura`, `${os.arch()}`, true)
+        .addField(`:rocket:  CPU`, `${os.cpus().map(i => `${i.model}`)[0]}`, true)
+        .addField(`:pager: RAM`, `${Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000)} MB / ${Math.trunc(os.totalmem() / 1024 / 1000)} MB (${Math.round((Math.round(process.memoryUsage().heapUsed / 1024 / 1024) / Math.round(os.totalmem() / 1024 / 1024)) * 100)}%)`, true)
+        .addField(`:dividers: Lib`, `Discord.js ${Discord.version}`, true)
+        .setTimestamp();
+        msg.channel.send(embed);
+    }
+};
