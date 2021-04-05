3d7af43b7a5bdc51eaebb8774b93d2cac18a4186
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-05 03:36:00 -0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
         embed.setColor('#FF0202').setTitle(`Estadisticas`)
-        .addField(`:desktop: So`, `${os.platform()}`, true)
+        .addField(`:desktop: Sistema Operativo`, `${os.platform()}`, true)
         .addField(`:gear: Arquitectura`, `${os.arch()}`, true)
         .addField(`:rocket:  CPU`, `${os.cpus().map(i => `${i.model}`)[0]}`, true)
         .addField(`:pager: RAM`, `${Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000)} MB / ${Math.trunc(os.totalmem() / 1024 / 1000)} MB (${Math.round((Math.round(process.memoryUsage().heapUsed / 1024 / 1024) / Math.round(os.totalmem() / 1024 / 1024)) * 100)}%)`, true)
