c31bed885b64142bcdeed60621384d3aebdf0ac0
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-09 12:31:00 -0300

    Update dato.js

--- commands/dato.js
+++ commands/dato.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'dato',
     description: 'que buen dato me has dado',
     execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/825444348958343228/dato.mp4");
+        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/830102375540457565/dato.mp4");
     }
 };
