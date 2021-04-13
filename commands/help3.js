8584d679cddeb27f05a5dd4c46b88cf2c81169eb
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-13 12:27:16 -0300

    Update help3.js

--- commands/help3.js
+++ commands/help3.js
@@ -7,7 +7,7 @@ module.exports = {
         const commands = ["eta", "ey", "siri", "messirve", "nomessirve", "clown", "poc", "vinonico"];
         const embed = new Discord.MessageEmbed()
         .setTitle('Comandos | 3/4')
-        .setColor('#199057')
+        .setColor('#26FF82')
         .setDescription(`Comandos disponibles | ${commands.length} en total`)
         .setFooter('ChitoPanBOT#4390', 'https://cdn.discordapp.com/attachments/795478872597594164/828373150382161970/a.jpg')
         .setTimestamp(new Date());
