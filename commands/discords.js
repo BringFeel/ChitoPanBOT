ea3577aba1bd81862766b76c8700f438ce110939
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-13 17:24:50 -0300

    Update discords.js

--- commands/discords.js
+++ commands/discords.js
@@ -6,8 +6,8 @@ module.exports = {
     description: 'Discords en los que está chitopanbot',
     execute(msg, args) {
       const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').setTitle(`Estoy en`)
-        .addField(`${client.guilds.cache.size}`, `discords`, true)
+        embed.setColor('#26FF82').setTitle(`Actualmente estoy en`)
+        .addField(`${client.guilds.cache.size}`, `servidores`, true)
         embed.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png')
       msg.channel.send(embed);
     }
