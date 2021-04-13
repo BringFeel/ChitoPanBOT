247adf741fb6208514e403c9b39eee868c129b56
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-13 10:20:50 -0300

    Update xxx.js

--- commands/xxx.js
+++ commands/xxx.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'que buscas pajin',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF0000').setTitle('Que buscas pajin').setImage('https://www.trecebits.com/wp-content/uploads/2015/08/tumblr_mmefxxPMO61qioyygo1_500.gif').setTimestamp();
+        embed.setColor('#FF0000').setTitle('Que buscas pajin').setImage('https://www.trecebits.com/wp-content/uploads/2015/08/tumblr_mmefxxPMO61qioyygo1_500.gif').setFooter("ChitoPanBOT#4390 • +mcsrvapi", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
         msg.channel.send(embed);
     }
 };
