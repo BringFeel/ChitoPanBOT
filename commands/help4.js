f6c15a977353a2dc5054435c9c8afbe633fd30ea
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-13 12:27:31 -0300

    Update help4.js

--- commands/help4.js
+++ commands/help4.js
@@ -7,7 +7,7 @@ module.exports = {
         const commands = ["buendia", "jueves", "dato", "bomdia", "whoping"];
         const embed = new Discord.MessageEmbed()
         .setTitle('Comandos | 4/4')
-        .setColor('#199057')
+        .setColor('#26FF82')
         .setDescription(`Comandos disponibles | ${commands.length} en total`)
         .setFooter('ChitoPanBOT#4390', 'https://cdn.discordapp.com/attachments/795478872597594164/828373150382161970/a.jpg')
         .setTimestamp(new Date());
