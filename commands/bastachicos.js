07d92e69387e73f14c9e82bc734ddd943d852cad
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-13 10:27:45 -0300

    Update bastachicos.js

--- commands/bastachicos.js
+++ commands/bastachicos.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Basta Chicos',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#FF0202').setTitle(`Basta Chicos`).setImage('https://cdn.discordapp.com/attachments/726437915197964338/802306022727352370/eyc9fJ.gif').setTimestamp();
+        embed.setColor('#FF0202').setTitle(`Basta Chicos`).setImage('https://cdn.discordapp.com/attachments/726437915197964338/802306022727352370/eyc9fJ.gif').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
         msg.channel.send(embed);
     }
 };
