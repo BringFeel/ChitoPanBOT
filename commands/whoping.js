b87c6ee37064b8b0fb0eb81e3b4dd09caa8d934e
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-13 12:29:37 -0300

    Update whoping.js

--- commands/whoping.js
+++ commands/whoping.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'quien chota pingueó',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#1500FF').setTitle(`Quien Chota Pingueó`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/827363440879206450/ping.jpg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+        embed.setColor('#26FF82').setTitle(`Quien Chota Pingueó`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/827363440879206450/ping.jpg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
         msg.channel.send(embed);
     }
 };
