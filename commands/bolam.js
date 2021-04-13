f6d3b334c1bd09c9e8bc87595261a3b9874ef759
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-13 12:24:28 -0300

    Update bolam.js

--- commands/bolam.js
+++ commands/bolam.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Paloma',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#00FBFF').setTitle(`Bolam la paloma`).setImage('https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_232801.jpg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+        embed.setColor('#26FF82').setTitle(`Bolam la paloma`).setImage('https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_232801.jpg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
         msg.channel.send(embed);
     }
 };
