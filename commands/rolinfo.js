de94f6c7f908d4e1916a94f3b2bd5f8ea0e3cce5
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-14 02:24:11 -0300

    Update rolinfo.js

--- commands/rolinfo.js
+++ commands/rolinfo.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(msg, args) {
         if (!msg.member.hasPermission(['MANAGE_ROLES'])) return msg.channel.send('¡No tienes permiso para ejecutar este comando!')
         const embed = new Discord.MessageEmbed()
-        .setAuthor('ChitoPanBot - Comandos', 'https://cdn.discordapp.com/attachments/795478872597594164/831749395309527040/homero_says_a.png', 'https://discord.gg/CzkSjzZ')
+        .setAuthor('ChitoPanBot - Permisos disponibles para cambiar', 'https://cdn.discordapp.com/attachments/795478872597594164/831749395309527040/homero_says_a.png', 'https://discord.gg/CzkSjzZ')
         .addField("___***Permisos:***___", "CREATE_INSTANT_INVITE" + "\nKICK_MEMBERS" + "\nBAN_MEMBERS" + "\nADMINISTRATOR" + "\nMANAGE_CHANNELS" + "\nMANAGE_GUILD" + "\nADD_REACTIONS", true)
         .addField("___***Permisos:***___", "READ_MESSAGES" + "\nSEND_MESSAGES" + "\nSEND_TTS_MESSAGES" + "\nMANAGE_MESSAGES" + "EMBED_LINKS" + "\nATTACH_FILES" + "\nREAD_msg_HISTORY", true)
         .addField("___***Permisos:***___", "MENTION_EVERYONE" + "\nEXTERNAL_EMOJIS" + "\nCONNECT" + "\nSPEAK" + "\nMUTE_MEMBERS" + "\nDEAFEN_MEMBERS" + "\nMOVE_MEMBERS", true)
@@ -14,6 +14,8 @@ module.exports = {
         .setColor('#26FF82')
         embed.setFooter('ChitoPanBOT#4390');
         embed.setTimestamp();
-    msg.channel.send(embed);
+    msg.author.send(embed);
+    if(msg.author.bot) return;
+    msg.channel.send(`${msg.author.toString()} Fijate tu md para ver los permisos!`)
     }
 };
