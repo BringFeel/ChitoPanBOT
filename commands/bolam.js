0b884fd6f8798f4f3e50bb521b41f662d2c8f3ba
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-14 20:53:52 -0300

    Update bolam.js

--- commands/bolam.js
+++ commands/bolam.js
@@ -2,10 +2,13 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'bolam',
-    description: 'Paloma',
+    description: 'Upa',
     execute(msg, args) {
-        const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').setTitle(`Bolam la paloma`).setImage('https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_232801.jpg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Mmmm jorge te pasaste de curioso')
+        .setColor('#26FF82')
+        embed.setFooter('ChitoPanBOT#4390');
+        embed.setTimestamp();
         msg.channel.send(embed);
     }
 };
