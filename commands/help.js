9eb49fcf36ba2622f8754d4bb70631e47ffc2c02
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-15 19:54:15 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
         .addField("___***Admin:***___", "**+rol**" + "\nCambia los permisos de un rol" + "\n**+rolinfo**" + "\nPermisos disponibles para cambiar", true)
         .addField("___***Minecraft:***___", "**+mcsrv**" + "\nPing a un servidor de minecraft" + "\n**+mcsrvapi**" + "\nAPIs usadas para +mcsrv", true)
         .addField("___***CounterStrike:***___", "**+cs**" + "\nInfo del servidor para Counter Strike 1.6" + "\n**+csgo**" + "\nInfo sobre tu cuenta de csgo", true)
-        .addField("___***Usuario:***___", "**+avatar**" + "\nMuestra el avatar del usuario mencionado", true)
+        .addField("___***Usuario:***___", "**+avatar**" + "\nMuestra el avatar del usuario mencionado" + "**+abrazo**" + "\nDale un abrazo a alguien :3", true)
         .addField("___***Random:***___", "**+bi**" + "\nPorcentaje aleatorio de bisexual" + "\n**+furry**" + "\nPorcentaje aleatorio de furry" + "\n**+gay**" + "\nPorcentaje aleatorio de gay"  + "\n**+otaku**" + "\nPorcentaje aleatorio de otaku", true)
         .setColor('#26FF82')
         embed.setFooter('Owner: Fran...#1904 • Gracias por usar ChitoPanBOT');
