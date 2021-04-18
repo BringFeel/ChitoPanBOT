4f72538a6df789f9f5ea728c54d02e7382fd39f7
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-17 23:39:44 -0300

    Update bomdia.js

--- commands/bomdia.js
+++ commands/bomdia.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Bom día caralho',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').setTitle(`Bom Día Caralho`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/824828488833171476/bom_dia.jpeg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+        embed.setColor('#26FF82').setTitle(`Bom Día Caralho`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/824828488833171476/bom_dia.jpeg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
