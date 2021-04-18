354e77e37750594acc6db508acd3c7f980ed2e57
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-17 23:34:42 -0300

    Update abrazo.js

--- commands/abrazo.js
+++ commands/abrazo.js
@@ -13,7 +13,7 @@ module.exports = {
 	const aC = new Discord.MessageEmbed()
     .setTitle(aA.tag+' abrazó a '+aB.tag)
     .setImage(star.hug())
-    aC.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+    aC.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
     aC.setColor('#2636FF')
 	msg.channel.send(aC);
     }
