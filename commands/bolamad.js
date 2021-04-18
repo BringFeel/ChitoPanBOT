cb4a64abfb1be3d330d1b29bb2e098de4a11ae68
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-17 23:39:19 -0300

    Update bolamad.js

--- commands/bolamad.js
+++ commands/bolamad.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'madolam',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').setTitle(`M  A  D  O  L  A  M`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/824836743005732864/Screenshot_20210325-234508_Discord.jpg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+        embed.setColor('#26FF82').setTitle(`M  A  D  O  L  A  M`).setImage('https://cdn.discordapp.com/attachments/795478872597594164/824836743005732864/Screenshot_20210325-234508_Discord.jpg').setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
