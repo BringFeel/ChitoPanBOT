b2820344626e7513a8d71dfe3a2c05e425dfe745
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-17 23:44:15 -0300

    Update mcsrvapi.js

--- commands/mcsrvapi.js
+++ commands/mcsrvapi.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'APIs usadas',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').addField("APIs usadas:", "https://mcapi.us/" + "\nhttps://api.mcsrvstat.us/", true).setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+        embed.setColor('#26FF82').addField("APIs usadas:", "https://mcapi.us/" + "\nhttps://api.mcsrvstat.us/", true).setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
