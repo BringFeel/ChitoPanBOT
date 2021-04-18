bab0b38a9578ef2b63e1a482c6bdae4c5c0c9176
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-17 23:43:55 -0300

    Update mcsrv.js

--- commands/mcsrv.js
+++ commands/mcsrv.js
@@ -40,7 +40,7 @@ module.exports = {
             } else {
                 embed.setTitle('El servidor no está online o sucedió algún error en el ping');
             }
-            embed.setFooter("ChitoPanBOT#4390 • +mcsrvapi", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+            embed.setFooter("ChitoPanBOT#4390 • +mcsrvapi", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
             msg.channel.send(embed);
             return;
         }
