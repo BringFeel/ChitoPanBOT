c9404f31e4c9d8b3408575f751828c230649fa24
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-17 23:43:08 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -8,7 +8,7 @@ module.exports = {
         embed.setColor('#26FF82').setTitle('ChitoPanBOT Invite:')
 	    .setURL('https://chitopanbotinvite.cobel-motion.xyz/')
 	    .setDescription('https://chitopanbotinvite.cobel-motion.xyz/')
-	    .setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png')
+	    .setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg')
 	    .setTimestamp();
         msg.channel.send(embed);
     }
