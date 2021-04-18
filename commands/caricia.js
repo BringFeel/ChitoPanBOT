2989b0067207d280e45b7034a8fd117ff692f2ea
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-18 02:23:56 -0300

    Add files via upload

---
+++ commands/caricia.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+const star = require('star-labs');
+
+module.exports = {
+    name: 'caricia',
+    description: 'UwU',
+    execute(msg, args) {
+        let command = 'caricia';
+        if(command === 'caricia'){
+	let aA = msg.author
+	let aB = msg.mentions.users.first()
+	if(!aB) return msg.channel.send(`${msg.author.toString()} Menciona a 1 usuario para darle una caricia.`);
+	const aC = new Discord.MessageEmbed()
+    .setTitle(aA.tag+' se encuentra acariciando a '+aB.tag)
+    .setImage(star.pat())
+    aC.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
+    aC.setColor('#2636FF')
+	msg.channel.send(aC);
+    }
+}
+};
---
+++ commands/sopapo.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+const star = require('star-labs');
+
+module.exports = {
+    name: 'sopapo',
+    description: 'XD',
+    execute(msg, args) {
+        let command = 'sopapo';
+        if(command === 'sopapo'){
+	let aA = msg.author
+	let aB = msg.mentions.users.first()
+	if(!aB) return msg.channel.send(`${msg.author.toString()} Menciona a 1 usuario para darle un sopapo.`);
+	const aC = new Discord.MessageEmbed()
+    .setTitle(aA.tag+' le encajó un sopapo a '+aB.tag)
+    .setImage(star.slap())
+    aC.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
+    aC.setColor('#2636FF')
+	msg.channel.send(aC);
+    }
+}
+};
