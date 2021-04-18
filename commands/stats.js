88b83003e63463e8c7d3512bf73d1e269f26f02b
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-17 23:47:46 -0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -24,7 +24,7 @@ module.exports = {
         .addField(`🏓Ping Bot`, `${Date.now() - msg.createdTimestamp}ms`, true)
         .addField(`🌐Ping API`, `${Math.round(client.ws.ping)}ms`, true)
         .addField(`⏱Tiempo online`, `${days} día/s , ${hours} hora/s, ${minutes} minuto/s y ${seconds} segúndo/s`, true)
-        embed.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/831323098497810443/e5c50561d108aa8732bd3bcd30473892_1.png').setTimestamp();
+        embed.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
