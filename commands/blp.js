ac356462166e9dfa523afe0520b0d2d411541feb
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-19 00:45:15 -0300

    Update blp.js

--- commands/blp.js
+++ commands/blp.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'Paloma',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#00FBFF').setTitle(`Bolam la paloma`).setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
+        embed.setColor('#00FBFF').setImage('https://cdn.discordapp.com/attachments/808513589223882802/820860165472387112/20210311_232801.jpg').setTitle(`Bolam la paloma`).setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
