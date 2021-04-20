95e37742844564e2b5c46ef092542186dd3acbb3
Fran <69062690+Fran2985@users.noreply.github.com>
2021-04-19 21:50:37 -0300

    Create turbo.js

---
+++ commands/turbo.js
@@ -0,0 +1,6 @@
+    name: 'turbo',
+    description: 'Epaa',
+    execute(msg, args) {
+        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/833865895863386182/Puse_el_turbo_pa.mp4");
+    }
+};
