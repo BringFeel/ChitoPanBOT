c2009ad9e88926bcf181c8c2d4708da0253d23fb
Fran <69062690+Fran2985@users.noreply.github.com>
2021-05-06 20:00:46 -0300

    Add files via upload

---
+++ commands/serverinfo.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'serverinfo',
+    description: 'Info del server',
+    execute(msg, args) {
+    var server = msg.guild;
+    
+    
+    const embed = new Discord.MessageEmbed()
+    .setTitle("**SERVERINFO**")
+    .setDescription("**INFORMACIÓN ACTUAL DEL SERVIDOR**")
+    .setThumbnail(server.iconURL())
+    .setAuthor(server.name, server.iconURL())
+    .addField('**FECHA DE CREACION**',`${server.createdAt}`)
+    .addField("**REGION**", msg.guild.region)
+    .addField("**DUEÑO DEL SERVIDOR**",`${server.owner.user}`)
+    .addField(`**CANALES** [${server.channels.cache.size}]ㅤㅤ`, `Categoria: ${server.channels.cache.filter(x => x.type === "category").size} texto: ${server.channels.cache.filter(x => x.type === "text").size} voz: ${server.channels.cache.filter(x => x.type === "voice").size}`, true)
+    .addField('**MIEMBROS**', server.memberCount, true)
+    .addField('**EMOJIS**',msg.guild.emojis.cache.size)
+    .addField('**BOOSTER**',msg.guild.premiumSubscriptionCount.toString())
+    .addField('**NIVEL DE VERIFICACION**',`${server.verificationLevel}`)
+    .addField('**ROLES**', server.roles.cache.size,true)
+    embed.setFooter(`ChitoPanBOT#4390 • Server ID: ${server.id}`, 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg')
+    .setTimestamp()
+    .setColor("#26FF82")
+    msg.channel.send(embed);
+}
+};
