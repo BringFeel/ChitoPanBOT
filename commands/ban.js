61a51f5aea614d1e1ab483a5a2b0731bdadf26a9
Fran <69062690+Fran2985@users.noreply.github.com>
2021-05-13 20:15:56 -0300

    Add files via upload

--- commands/ban.js
+++ commands/ban.js
@@ -1,12 +1,42 @@
-
 const Discord = require('discord.js');
 
 module.exports = {
     name: 'ban',
-    description: 'ban al que se hace el piola',
-    execute(msg, args) {
-        const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').setTitle(`Que hacés pibe`).setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
-        msg.channel.send(embed);
+    description: 'sos pollo',
+    execute(msg, args, client) {
+
+        if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send('No tienes permisos para usar este comando.')
+        if(!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.send('No tengo los permisos suficientes para este comando.')
+
+        const member = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]);
+
+        if(!args[0]) return msg.channel.send('Por favor especifica un usuario');
+
+        if(!member) return msg.channel.send('Parece que no puedo encontrar a este usuario.');
+        if(!member.bannable) return msg.channel.send('Este usuario no puede ser baneado, su rol más alto es más alto que el mío.');
+
+        if(member.id === msg.author.id) return msg.channel.send('Boe, no te podes banear a vos mismo');
+
+        let reason = args.slice(1).join(" ");
+
+        if(!reason) reason = 'Sin especificar';
+
+        member.ban({ reason: `${reason}` }).catch(err => { 
+          msg.channel.send('Upa, Algo salió mal')
+            console.log(err)
+        })
+
+        const banembed = new Discord.MessageEmbed()
+        .setColor("#26FF82")
+        .setTitle('Miembro baneado')
+        .addField('Usuario Baneado', member)
+        .addField('Baneado por', msg.author)
+        .addField('Rasón', reason)
+        .setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg')
+        .setTimestamp()
+
+        msg.channel.send(banembed);
+
+
     }
-};
+}
