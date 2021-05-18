32e304eb5edf9d22e1726fc8c278f8f259ddc80a
Fran <69062690+Fran2985@users.noreply.github.com>
2021-05-18 00:01:43 -0300

    Update servidores.js

--- commands/servidores.js
+++ commands/servidores.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Total de servidores de discord en los que está chitopanbot',
     execute(msg, args) {
       const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').setTitle(`Actualmente estoy en`)
+        embed.setColor('#26FF82')
         .addField(`**Servidores:**`, `${client.guilds.cache.size}\n**Usuarios:**\n${client.users.cache.size}`, true)
         embed.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg')
       msg.channel.send(embed);
