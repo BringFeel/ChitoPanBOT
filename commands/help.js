b610a6cd5f831ac7c15f6947755602066f0c7686
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:48:43 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,13 +5,13 @@ module.exports = {
     description: 'Lista de los comandos disponibles.',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed()
-        .setAuthor('ChitoPanBot - Comandos', 'https://media.discordapp.net/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg', 'https://discord.gg/CzkSjzZ')
+        .setAuthor('ChitoPanBot - Comandos', 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg', 'https://discord.gg/CzkSjzZ')
         .addField("**🎵 Musica:**", "`+clear-queue` `+filter` `+loop` `+nowplaying` `+pause` `+play` `+queue` `+resume` `+search` `+shuffle` `+skip` `+stop` `+volume` `+w-filters`", true)
         .addField("**🎚Filtros:**", "`8d` `gate` `haas` `phaser` `treble` `tremolo` `vibrato` `reverse` `karaoke` `flanger` `mcompand` `pulsator` `subboost` `bassboost` `vaporwave` `nightcore` `normalizer` `surrounding`", true)
         .addField("**🛡Moderación:**", "`+ban` ```Soon: +unban +kick +mute +unmute```", true)
         .addField("**🔎Otros Comandos:**", "`+help` `+invite` `+servidores` `+stats` `+ping` `+mcsrv` `+mcsrvapi` `+csgo` `+hm` `+avatar` `+abrazo` `+beso` `+caricia` `+lamida` `+sopapo` `+serverinfo` `+miembros` `+rol` `+rolinfo` `+bi` `+furry` `+gay` `+otaku` `+trapo`", true)
         .setColor('#26FF82')
-        embed.setFooter('Owner: Bring Feel#2098 • Gracias por usar ChitoPanBOT', 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg');
+        embed.setFooter('Owner: Bring Feel#2098 • Gracias por usar ChitoPanBOT', 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg');
         embed.setTimestamp();
         msg.author.send(embed);
     if(msg.author.bot) return;
