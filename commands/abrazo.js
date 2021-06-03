2a18da522e6d17394e8f0ded71df1e73ce474407
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:43:57 -0300

    Update abrazo.js

--- commands/abrazo.js
+++ commands/abrazo.js
@@ -13,7 +13,7 @@ module.exports = {
 	const aC = new Discord.MessageEmbed()
     .setTitle(aA.tag+' abrazó a '+aB.tag)
     .setImage(star.hug())
-    aC.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
+    aC.setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg').setTimestamp();
     aC.setColor('#2636FF')
 	msg.channel.send(aC);
     }
