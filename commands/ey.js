1dd0c34cb95e611e7467bf540a396a0057462ed6
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:41:56 -0300

    Delete ey.js

--- commands/ey.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ey',
-    description: 'eyeyey',
-    execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/779878778842513449/816130764026282014/eyeyey.mp4");
-    }
-};
