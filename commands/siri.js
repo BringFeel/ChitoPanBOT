64b57a0f2dd6f0b25407e6b006ad0d02da522ef8
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:38:32 -0300

    Delete siri.js

--- commands/siri.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    name: 'siri',
-    description: 'Siri fazendo barra! incrivel',
-    execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/779878778842513449/816141461430337546/siri.mp4");
-    }
-};
