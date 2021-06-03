5e866132045ceebb15477b5e34928b1cc8b3a8e8
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:40:58 -0300

    Delete calmese.js

--- commands/calmese.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'calmese',
-    description: 'Calmese boludo',
-    execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/817118007099654154/Calmese-Boludo.mp4");
-    }
-};
