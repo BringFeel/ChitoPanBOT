65b42026584d542e17bc729ae43110036ddb237c
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:42:08 -0300

    Delete jueves.js

--- commands/jueves.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'jueves',
-    description: 'Feliz jueves',
-    execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/795478872597594164/821767135600443442/Asuka__Feliz_Jueves_HD_60fps720P_HD.mp4");
-    }
-};
