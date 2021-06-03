7376c995e2847453611142b3722a5d65df8d9ef5
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:57:19 -0300

    Update mcsrvapi.js

--- commands/mcsrvapi.js
+++ commands/mcsrvapi.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'APIs usadas',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
-        embed.setColor('#26FF82').addField("APIs usadas:", "https://mcapi.us/" + "\nhttps://api.mcsrvstat.us/", true).setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/795478872597594164/833157862825787412/91ed566974e33b4bd8d7506861b3d5de.jpg').setTimestamp();
+        embed.setColor('#26FF82').addField("APIs usadas:", "https://mcapi.us/" + "\nhttps://api.mcsrvstat.us/", true).setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg').setTimestamp();
         msg.channel.send(embed);
     }
 };
