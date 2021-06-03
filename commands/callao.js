f4e8689fdd0456787169c50708bf345e474dbd9d
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:40:49 -0300

    Delete callao.js

--- commands/callao.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'callao',
-    description: 'Callao lacra',
-    execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/726437915197964338/814127959140859924/CALLAO_LACRA.mp4");
-    }
-};
