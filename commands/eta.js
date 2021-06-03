6f5444ad8c7b777d0b3955cca6ee95f0cbc60ec0
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:41:35 -0300

    Delete eta.js

--- commands/eta.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'eta',
-    description: 'eta poooo',
-    execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/669301749009350656/812144372794654740/ae6961f7be174dcba005caa82b26eda1.mp4");
-    }
-};
