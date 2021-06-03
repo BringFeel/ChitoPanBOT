6e3c83112c3cf4df77283058afb6beec4587f914
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-03 20:37:56 -0300

    Delete sus.js

--- commands/sus.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'sus',
-    description: 'A M O N G U S',
-    execute(msg, args) {
-        msg.channel.send("https://cdn.discordapp.com/attachments/831975734070476861/836259044175642654/sus.mp4");
-    }
-};
