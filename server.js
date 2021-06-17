e52a3a1dcb8864bbde576ab0343e1691e612d8d4
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-06-17 19:31:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ client.on("guildCreate", guild => {
     .addField("Nombre:", `${guild.name}`)
     .addField("ID:", `${guild.id}`)
     .addField("Miembros:", `${guild.memberCount}`)
-    .addField("Región", guild.guild.region)
+    .addField("Región", message.guild.region)
     .addField("Dueño del servidor",`${guild.owner.user}`)
     .setColor("RANDOM")
     .setFooter(`Me agregaron a un nuevo servidor | Servidores totales: ${client.guilds.cache.size}`);
