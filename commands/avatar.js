608829185496c459cb2fa901d6dc057d3f607ee3
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-08-03 20:53:48 -0300

    Delete commands directory

--- commands/avatar.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'avatar',
-    aliases: ['av'],
-    description: 'Muestra tu avatar o el de otro usuario',
-    execute(msg, args) {
-        const avatarEmbed = require('discord.js-avatar');
-        avatarEmbed(msg, language = 'spanish');
-    }
-};
