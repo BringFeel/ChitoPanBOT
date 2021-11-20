4be6bc8ecbd441d50fcbb0f5938c4eb6db2ab886
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-11-20 17:16:04 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,8 +6,8 @@ module.exports = {
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
         embed.setColor('#26FF82').setTitle('ChitoPanBOT Invite:')
-	    .setURL('https://chitopanbotinvite.cobel-motion.xyz/')
-        embed.addField("Link de invitación:", "[>>LINK<<](https://chitopanbotinvite.cobel-motion.xyz/)")
+	    .setURL('https://discord.com/oauth2/authorize?client_id=779841907484262421&scope=bot&permissions=0/')
+        embed.addField("Link de invitación:", "[>>LINK<<](https://discord.com/oauth2/authorize?client_id=779841907484262421&scope=bot&permissions=0/)")
 	    .setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg')
 	    .setTimestamp();
         msg.channel.send(embed);
