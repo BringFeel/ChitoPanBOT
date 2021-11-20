2df246e81e039dac181c26bf7d238f36249ca581
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-11-20 17:17:28 -0300

    Update support.js

--- commands/bot/support.js
+++ commands/bot/support.js
@@ -6,8 +6,8 @@ module.exports = {
     execute(msg, args) {
         const embed = new Discord.MessageEmbed();
         embed.setColor('#26FF82').setTitle('Support:')
-	    .setURL('https://discord.gg/MHfKz6aYpx')
-        embed.addField("Discord Support:", "[>>LINK<<](https://discord.gg/MHfKz6aYpx)")
+	    .setURL('https://discord.com/invite/dZfPApM3ve')
+        embed.addField("Discord Support:", "[>>LINK<<](https://discord.com/invite/dZfPApM3ve)")
 	    .setFooter("ChitoPanBOT#4390", 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg')
 	    .setTimestamp();
         msg.channel.send(embed);
