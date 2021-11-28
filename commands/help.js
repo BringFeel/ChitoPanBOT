6404f4d2fb69ec2851527641d16023a84e1aafc5
BringFeel <69062690+Fran2985@users.noreply.github.com>
2021-11-27 21:46:53 -0300

    Update help.js
    Nombre del autor y link de soporte actualizado

--- commands/help.js
+++ commands/help.js
@@ -5,11 +5,11 @@ module.exports = {
     description: 'Lista de los comandos disponibles.',
     execute(msg, args) {
         const embed = new Discord.MessageEmbed()
-        .setAuthor('ChitoPanBot - Comandos', 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg', 'https://discord.gg/CzkSjzZ')
+        .setAuthor('ChitoPanBot - Comandos', 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg', 'https://cpbs.cobelmotion.com.ar/')
         .addField("**🛡Moderación:**", "`+ban` ```Soon: +unban +kick +mute +unmute```", true)
         .addField("**🔎Otros Comandos:**", "`+help` `+invite` `+servidores` `+stats` `+ping` `+mcsrv` `+mcsrvapi` `+csgo` `+hm` `+avatar` `+abrazo` `+beso` `+caricia` `+lamida` `+sopapo` `+serverinfo` `+miembros` `+rol` `+rolinfo` `+bi` `+furry` `+gay` `+otaku` `+trapo`", true)
         .setColor('#26FF82')
-        embed.setFooter('Owner: Bring Feel#2098 • Gracias por usar ChitoPanBOT', 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg');
+        embed.setFooter('Owner: BringFeel#8805 • Gracias por usar ChitoPanBOT', 'https://cdn.discordapp.com/attachments/831975734070476861/850154574596800592/cpbpfp.jpg');
         embed.setTimestamp();
         msg.author.send(embed);
     if(msg.author.bot) return;
